c3674f595a82be680aa1c7ad6e96004fa98e274c
Anderson Ignacio da Silva <anderson@aignacio.com>
2025-02-16 14:05:06 +0000

    Added more caches to test to increase coverage
    Signed-off-by: Anderson Ignacio da Silva <anderson@aignacio.com>

--- tests/test_cache_model.py
+++ tests/test_cache_model.py
@@ -101,17 +101,20 @@ def test_cache_comparison():
     seed = 42
 
     caches.append(DirectMappedCache(cache_line_bytes=64))
-    caches.append(DirectMappedCache(cache_size_kib=1, cache_line_bytes=64))
-    caches.append(SetAssociativeCache(n_way=4, cache_size_kib=4, cache_line_bytes=64))
     caches.append(
-    SetAssociativeCache(
-    n_way=4, cache_size_kib=4, replacement_policy=ReplacementPolicy.LRU
-    )
+        SetAssociativeCache(
+            n_way=4, cache_size_kib=4, replacement_policy=ReplacementPolicy.RANDOM
+        )
     )
     caches.append(
-    SetAssociativeCache(
-    n_way=4, cache_size_kib=4, replacement_policy=ReplacementPolicy.NMRU
+        SetAssociativeCache(
+            n_way=4, cache_size_kib=4, replacement_policy=ReplacementPolicy.LRU
+        )
     )
+    caches.append(
+        SetAssociativeCache(
+            n_way=4, cache_size_kib=4, replacement_policy=ReplacementPolicy.NMRU
+        )
     )
     caches.append(
         SetAssociativeCache(
